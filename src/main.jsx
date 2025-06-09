efbe1999e775de9a9926beded02c6aab0dff777e
Abd Eltawab Shaaban <105607290+scob337@users.noreply.github.com>
2025-06-09 21:11:58 +0300

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Services from './Pages/Services'
-import ServiceDetail from './Pages/ServiceDetail'
-// import About from './Pages/About'
-import Layout from './Components/layout'
-import Contact from './Pages/Contact';
-import Home from './Pages/Home';
-import About from './Pages/About'
 import './i18n/i18n';
+import LanguageSwitcher from './Components/LanguageSwitcher';
+import Layout from './Components/Layout';
+import Home from './Pages/Home';
+import Services from './Pages/Services';
+import About from './Components/Home/About';
+import ServiceDetail from './Pages/ServiceDetail';
+import Contact from './Pages/Contact';
 
 const router = createBrowserRouter([
   {
@@ -44,7 +44,6 @@ const router = createBrowserRouter([
   }
 ])
 
-import LanguageSwitcher from './Components/LanguageSwitcher';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
